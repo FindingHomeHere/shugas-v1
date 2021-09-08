5db4003f57ddddc68cfabf45861bd422b50c4e1b
Isaac Johnson <isaac.johnson.2493@gmail.com>
2021-09-08 11:32:26 -0600

    Removed function call for SMTP in application page

--- src/pages/api/v1/apps/index.js
+++ src/pages/api/v1/apps/index.js
@@ -59,13 +59,13 @@ const createApp = async (req, res) => {
             resume,
           });
           const adminPortal = `${req.protocol}://${req.headers.host}/admin`;
-          const applicationPage = `${req.protocol}://${req.headers.host}/jobs`;
+          // const applicationPage = `${req.protocol}://${req.headers.host}/jobs`;
           const kev = {
             name: 'Kevin Dexter',
             email: 'kevinjdexter@icloud.com',
           };
 
-          await new Email(doc, applicationPage).sendApplicationConfirmation();
+          // await new Email(doc, applicationPage).sendApplicationConfirmation();
           await new Email(kev, adminPortal).sendApplicationNotification();
         }
 
