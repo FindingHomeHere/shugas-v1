6a4ae975f1a6bdf9ee88446eab8c1b6bcf3fff5c
Isaac Johnson <isaac.johnson.2493@gmail.com>
2023-01-12 11:08:42 -0700

    optimised image size for reserve page

--- src/pages/reserve.jsx
+++ src/pages/reserve.jsx
@@ -1,5 +1,5 @@
-import { Flex, Stack, Image } from '@chakra-ui/react';
-// import Image from 'next/image';
+import { Flex, Stack } from '@chakra-ui/react';
+import Image from 'next/image';
 import React from 'react';
 
 const Reservations = () => {
